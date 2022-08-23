cfe9afb173c24f11414ba7ae9eb5063acf12d427
ktjs7252 <ktjs7252@naver.com>
2022-08-23 23:29:25 +0900

    'first'

---
+++ reviews/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    """Review Admin Definiton"""
+
+    list_display = (
+        "__str__", "rating_average"
+    )
---
+++ rooms/admin.py
@@ -0,0 +1,78 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+
+
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+
+    """Item Admin Definition"""
+
+    list_display = (
+        "name", "used_by"
+    )
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+
+    """Room Model Definition"""
+
+    fieldsets = (
+        (
+            "Spaces",
+            {"fields": ("guests", "beds", "bedrooms", "bath")}
+        ),
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")}
+        ),
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book")}
+        ),
+        (
+            "More About the Space",
+            {"fields": ("amenities", "facilities", "house_rules")}
+        ),
+        (
+            "Last Details",
+            {"fields": ("host",)}
+        )
+    )
+    list_display = (
+        "name", "country", "city", "price", "guests",
+        "beds", "bedrooms", "bath", "check_in", "check_out", "instant_book",
+        "count_amenities", "count_photos","total_rating",
+    )
+
+    list_filter = (
+        "instant_book", "host__superhost", "room_type", "amenities",
+        "facilities", "house_rules", "country", "city",
+    )
+
+    search_fields = (
+        "=city", "^host__username",
+    )
+
+    filter_horizontal = (
+        "amenities", "facilities", "house_rules",
+    )
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """"""
+
+    pass
